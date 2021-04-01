1b03266ea20fab211e9a7cdf8293cade9580377c
insaniee <insanie232@gmail.com>
2021-04-01 15:54:05 +0300

    deleted-annesasary-files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import "./App.css";
-import ToysApp from "./comps/toysComps/toysApp";
-import AppToDo from "./comps_hw_todo/appTodo";
-import ConverterApp from "./converterComps/converter";
 import CurrencyApp from "./converterComps/currencyApp";
 
 function App() {
